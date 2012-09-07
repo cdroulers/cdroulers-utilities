ac0a22c4360e07b798be5b88d6f57eadd36059ee
cdroulers <cdroulers@SHE-PC-1184.dns1.sherweb.com>
2012-09-07 12:00:56 -0400

    Add utility script for PowerShell SVN access.

---
+++ scripts/svn.ps1
@@ -0,0 +1,42 @@
+<#    
+    .PARAMETER Path
+    Which Path to use
+    .PARAMETER Log
+    Shows the history panel
+#>
+
+PARAM(
+    [Parameter(HelpMessage = "Which path to run commands on",
+		ValueFromPipeline = $True)]
+    [String]
+    $Path = ".",
+    [Parameter(HelpMessage = "Show the log interface (history)")]
+    [Switch]
+    $Log = $False,
+    [Parameter(HelpMessage = "Show the diff interface")]
+    [Switch]
+    $Diff = $False,
+    [Parameter(HelpMessage = "Show the properties interface")]
+    [Switch]
+    $Properties = $False
+)
+
+if ($Log)
+{
+    & TortoiseProc.exe /command:log /path:$Path
+	Exit;
+}
+
+if ($Diff)
+{
+    & TortoiseProc.exe /command:diff /path:$Path
+	Exit;
+}
+
+if ($Properties)
+{
+    & TortoiseProc.exe /command:properties /path:$Path
+	Exit;
+}
+
+Write-Warning "No command specified";
